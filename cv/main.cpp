5829ef2c4097b20fe761669d93129ec548753727
Cole Brinsfield <gizmosrock@gmail.com>
2016-01-13 13:34:49 -0800

    Prints out coords of object with largest detected object.

--- cv/main.cpp
+++ cv/main.cpp
@@ -4,8 +4,10 @@
 #include "stdio.h"
 #include <iostream>
 #include <stack>
+
 using namespace cv;
-int hl,sl,vl,vh,sh,hh;
+int highestarea,highestindex,counter,x,y;
+
 void Traverse(int xs, int ys, cv::Mat &ids,cv::Mat &image, int blobID, cv::Point &leftTop, cv::Point &rightBottom) {
     std::stack<cv::Point> S;
     S.push(cv::Point(xs,ys));
@@ -67,42 +69,70 @@ int main(int, char**)
     VideoCapture cap(0); // open the default camera
     if(!cap.isOpened())  // check if we succeeded
         return -1;
+    cap.set(CV_CAP_PROP_FRAME_WIDTH,640);
+    cap.set(CV_CAP_PROP_FRAME_HEIGHT,480);
+    cap.set(CV_CAP_PROP_BUFFERSIZE, 300);
     Mat edges;
     for(;;)
     {
+    	double t = (double)cv::getTickCount();
         std::vector<Rect> keypoints;
     	Mat im_with_keypoints;
         Mat frame;
         cap >> frame; // get a new frame from camera
-        resize(frame,frame,frame.size()*2); //double the size (not sure if needed anymore)
+        resize(frame,frame,frame.size()*1); //double the size (not sure if needed anymore)
         frame.copyTo(edges); //copy it to edges for post processings
         frame.copyTo(im_with_keypoints); // copy to im_with_keypoints for the final drawn image
         inRange(edges, cv::Scalar(222, 255, 0), cv::Scalar(255, 255, 255), edges); //Find green reflection only (pretty good rgb)
         erode(edges, edges, 0, Point(-1, -1), 2, 1, 1); //Make the edges cleaner
         FindBlobs(edges,keypoints,0); //Use our FindBlobs function, which looks for white blobs only
+        highestarea=10000;
+        x=-1;
+        y=-1;
         for(int i=1;i<keypoints.size();i++){
         	rectangle(im_with_keypoints,keypoints[i],Scalar(0,0,255)); //For each of our keypoints (detected objects), draw the square around it.
+        	if(keypoints[i].area()>highestarea){
+        		highestarea=keypoints[i].area();
+        		x=keypoints[i].x+(keypoints[i].width)/2);
+        		y=keypoints[i].y+(keypoints[i].height/2);
+        	}
+
         }
-    	imshow("keypoints", im_with_keypoints ); //show us the image with squares
-        imshow("edges", edges); //show us just the detected objects
+        if(counter%2==0){
+        	std::cout<<"Highest Area is "<<highestarea<<" with an x y of "<<x<<" "<<y<<std::endl;
+        	std::cout<<cap.get(CV_CAP_PROP_FRAME_COUNT)<<std::endl;;
+        }
+
+        counter++;
+        if(counter==2){
+        	imshow("keypoints", im_with_keypoints ); //show us the image with squares
+        	imshow("edges", edges); //show us just the detected objects
+        	counter=0;
+        }
         if(waitKey(30) >= 0) break; //press a key to quit
 
     }
     return 0;
 }
-int socketConnection(){
-	try
-	  {
-		// Create the client socket
-		ClientSocket client_socket ( "localhost", 30000 );
-
-		// rest of code -
-		// send request, retrieve reply, etc...
-
-	  }
-	catch ( SocketException& e )
-	  {
-		std::cout << "Exception was caught:" << e.description() << "\n";
-	  }
-	return 0;
-}
+//int socketConnection(){
+//	try
+//	  {
+//		// Create the client socket
+//		ClientSocket client_socket ( "localhost", 30000 );
+//		try{
+//			client_socket << ""+
+//		}
+//		catch ( SocketException& ) {
+//
+//		}
+//
+//		// rest of code -
+//		// send request, retrieve reply, etc...
+//
+//	  }
+//	catch ( SocketException& e )
+//	  {
+//		std::cout << "Exception was caught:" << e.description() << "\n";
+//	  }
+//	return 0;
+//}
