ac9f11e4b5215cfdd21b9b018f9d5f1394d56323
Nathan Larson <Nathan Larson>
2016-01-13 17:12:19 -0800

    Added intake and xbox things

---
+++ SampleDrive/src/modules/IntakeModule.h
@@ -0,0 +1,24 @@
+/*
+ * IntakeModule.h
+ *
+ *  Created on: Jan 13, 2016
+ *      Author: natek
+ */
+
+#ifndef SRC_MODULES_INTAKEMODULE_H_
+#define SRC_MODULES_INTAKEMODULE_H_
+
+#include <WPIlib.h>
+
+class IntakeModule {
+	IntakeModule(int intake);
+	void setPower(double pow);
+	void spinIn();
+	void spinOut();
+
+private:
+	CANTalon* intakeTalon;
+};
+
+
+#endif /* SRC_MODULES_INTAKEMODULE_H_ */
