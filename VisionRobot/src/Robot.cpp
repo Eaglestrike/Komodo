a2196c9c81a6f4482f91a12e79c8281997e55c59
Cole Brinsfield <gizmosrock@gmail.com>
2016-01-16 00:40:57 -0800

    Added XBox servo control, should help with logic finding and stuff

--- VisionRobot/src/Robot.cpp
+++ VisionRobot/src/Robot.cpp
@@ -1,12 +1,19 @@
 #include "WPILib.h"
 #include "NetworkTables/NetworkTable.h"
 //#include "HUDServer.h"
+#include "Xbox.h"
 
 class Robot: public IterativeRobot
 {
 public:
 	std::shared_ptr<NetworkTable> visionTable;
 private:
+	double movementInX=0;
+	Xbox* xbox;
+
+	double movementInY=0;
+	double x;
+	double y;
 	LiveWindow *lw = LiveWindow::GetInstance();
 	double frameWidth,frameHeight,xMovePerTick,yMovePerTick,tolerance;
 	int movementFactor=1;
@@ -16,7 +23,7 @@ private:
 
 	void RobotInit()
 	{
-
+		xbox = new Xbox(2);
 		visionTable = NetworkTable::GetTable("visionTable");
 		up = new Servo(0);
 		side = new Servo(1);
@@ -42,20 +49,22 @@ private:
 		tolerance = visionTable->GetNumber("Tolerance");
 		yMovePerTick = visionTable->GetNumber("yTicks");
 		xMovePerTick = visionTable->GetNumber("xTicks");
+
 		up->SetAngle(0);
 		side->SetAngle(0);
 	}
 
 	void TeleopPeriodic()
 	{
+		movementInX=0;
+		movementInY=0;
 		std::cout<<visionTable->GetNumber("detectedObject")<<std::endl;
 		if(visionTable->GetNumber("detectedObject")==1){
 			std::cout<<"Saw an object"<<std::endl;
 
-			int x = visionTable->GetNumber("X");
-			int y = visionTable->GetNumber("Y");
-			double movementInX=0;
-			double movementInY=0;
+			x = visionTable->GetNumber("X");
+			y = visionTable->GetNumber("Y");
+
 			if(x>frameWidth/2+tolerance){
 				movementInX = xMovePerTick;
 			}
@@ -68,9 +77,6 @@ private:
 			if(y<frameWidth/2-tolerance){
 				movementInY=yMovePerTick;
 			}
-			std::cout<<"movementInX: "<<movementInX<<" movementInY: "<<movementInY<<std::endl;
-			std::cout<<"Current Y Angle: "<<up->GetAngle()<<" Current X Angle: "<<side->GetAngle()<<std::endl;
-			std::cout<<"X: "<<x<<" Y: "<<y<<std::endl;
 
 			up->SetAngle(up->GetAngle()-movementInY);
 			side->SetAngle(side->GetAngle()-movementInX);
@@ -91,11 +97,29 @@ private:
 
 			side->SetAngle(side->GetAngle()+xMovePerTick*movementFactor);
 		}
+		std::cout<<"movementInX: "<<movementInX<<" movementInY: "<<movementInY<<std::endl;
+		std::cout<<"Current Y Angle: "<<up->GetAngle()<<" Current X Angle: "<<side->GetAngle()<<std::endl;
+		std::cout<<"X: "<<x<<" Y: "<<y<<std::endl;
 	}
 
 	void TestPeriodic()
 	{
+
 		lw->Run();
+		if(xbox->getA()){
+			up->SetAngle(up->GetAngle()-1);
+		}
+		if(xbox->getY()){
+			up->SetAngle(up->GetAngle()+1);
+		}
+		if(xbox->getB()){
+			up->SetAngle(side->GetAngle()+1);
+		}
+		if(xbox->getX()){
+			up->SetAngle(side->GetAngle()-1);
+		}
+		std::cout<<"Current Y Angle: "<<up->GetAngle()<<" Current X Angle: "<<side->GetAngle()<<std::endl;
+
 	}
 };
 
