c42666d63e802671836e7ee5d13817ae18b620e1
icecube45 <gizmosrock@gmail.com>
2016-01-22 11:23:13 -0800

    updated this on web so i can pull

--- VisionRobot/src/Robot.cpp
+++ VisionRobot/src/Robot.cpp
@@ -2,20 +2,26 @@
 #include "NetworkTables/NetworkTable.h"
 //#include "HUDServer.h"
 #include "Xbox.h"
-
+#include <math.h>
 class Robot: public IterativeRobot
 {
 public:
 	std::shared_ptr<NetworkTable> visionTable;
 private:
 	double movementInX=0;
+	double xTime;
+	double yTime;
 	Xbox* xbox;
-
+	double xAngle;
+	Timer* xTimer;
+	Timer* yTimer;
+	double yAngle;
+	int ticks=0;
 	double movementInY=0;
 	double x;
 	double y;
 	LiveWindow *lw = LiveWindow::GetInstance();
-	double frameWidth,frameHeight,xMovePerTick,yMovePerTick,tolerance;
+	double frameWidth,frameHeight,xMovePerTick,yMovePerTick,Xtolerance, Ytolerance;
 	int movementFactor=1;
 	Servo* up;
 	Servo* side;
@@ -29,7 +35,12 @@ private:
 		std::cout << "hello" << std::endl;
 		up = new Servo(0);
 		side = new Servo(1);
+<<<<<<< HEAD
+		xTimer = new Timer();
+		yTimer =  new Timer();
+=======
 		std::cout << "hello" << std::endl;
+>>>>>>> aa09af6a27a6a8dc230459eefea14a282b69a78d
 		//server = new HUDServer(500, up, side);
 
 	}
@@ -50,43 +61,66 @@ private:
 	{
 		frameWidth = visionTable->GetNumber("width");
 		frameHeight = visionTable->GetNumber("height");
-		tolerance = visionTable->GetNumber("Tolerance");
+		Xtolerance = visionTable->GetNumber("XTolerance");
+		Ytolerance = visionTable->GetNumber("YTolerance");
 		yMovePerTick = visionTable->GetNumber("yTicks");
 		xMovePerTick = visionTable->GetNumber("xTicks");
-
-		up->SetAngle(0);
+		xTimer->Reset();
+		yTimer->Reset();
+		xTimer->Start();
+		yTimer->Start();
+		up->SetAngle(60);
 		side->SetAngle(0);
 	}
 
 	void TeleopPeriodic()
 	{
+
+		Xtolerance = visionTable->GetNumber("XTolerance");
+		Ytolerance = visionTable->GetNumber("YTolerance");
+
 		movementInX=0;
 		movementInY=0;
 		std::cout<<visionTable->GetNumber("detectedObject")<<std::endl;
 		if(visionTable->GetNumber("detectedObject")==1){
-			std::cout<<"Saw an object"<<std::endl;
+			std::cout<<"ticks: "<<ticks<<std::endl;
 
 			x = visionTable->GetNumber("X");
 			y = visionTable->GetNumber("Y");
 
-			if(x>frameWidth/2+tolerance){
-				movementInX = xMovePerTick;
+//			if(x>(frameWidth/2+Xtolerance)){
+//				movementInX = -xMovePerTick;
+//			}
+//			if(x<(frameWidth/2-Xtolerance)){
+//				movementInX = xMovePerTick;
+//			}
+//			if(y>(frameHeight/2+Ytolerance)){
+//				movementInY=yMovePerTick;
+//			}
+//			else if(y<(frameHeight/2-Ytolerance)){
+//				movementInY=-yMovePerTick;
+//			}
+			//0.19sec/60degrees move speed on servos
+			if(xTimer->Get()>=xTime){
+				xAngle = visionTable->GetNumber("xAngle");
+				double moveAngle = xAngle-26.4;
+				side->SetAngle(side->GetAngle()+moveAngle);
+				xTime = abs(double((.19/60)*moveAngle));
+				xTimer->Reset();
+				xTimer->Start();
 			}
-			if(x<frameWidth/2-tolerance){
-				movementInX = -xMovePerTick;
+			if(yTimer->Get()>=yTime){
+				yAngle = visionTable->GetNumber("yAngle");
+				double moveAngle = yAngle-26.4;
+				up->SetAngle(up->GetAngle()+moveAngle);
+				yTime = abs(double((.19/60)*moveAngle));
+				yTimer->Reset();
+				yTimer->Start();
 			}
-			if(y>frameWidth/2+tolerance){
-				movementInY=-yMovePerTick;
-			}
-			if(y<frameWidth/2-tolerance){
-				movementInY=yMovePerTick;
-			}
-
-			up->SetAngle(up->GetAngle()-movementInY);
-			side->SetAngle(side->GetAngle()-movementInX);
 			if(up->GetAngle()>90){
 				up->SetAngle(90);
 			}
+			ticks++;
 		}
 		else{
 
@@ -99,11 +133,11 @@ private:
 			}
 			std::cout<<side->GetAngle()<<std::endl;
 
-			side->SetAngle(side->GetAngle()+xMovePerTick*movementFactor);
+			side->SetAngle(side->GetAngle()+xMovePerTick*movementFactor*2);
 		}
-		std::cout<<"movementInX: "<<movementInX<<" movementInY: "<<movementInY<<std::endl;
-		std::cout<<"Current Y Angle: "<<up->GetAngle()<<" Current X Angle: "<<side->GetAngle()<<std::endl;
-		std::cout<<"X: "<<x<<" Y: "<<y<<std::endl;
+//		std::cout<<"movementInX: "<<movementInX<<" movementInY: "<<movementInY<<std::endl;
+//		std::cout<<"Current Y Angle: "<<up->GetAngle()<<" Current X Angle: "<<side->GetAngle()<<std::endl;
+//		std::cout<<"X: "<<x<<" Y: "<<y<<std::endl;
 	}
 
 	void TestPeriodic()
@@ -117,10 +151,10 @@ private:
 			up->SetAngle(up->GetAngle()+1);
 		}
 		if(xbox->getB()){
-			up->SetAngle(side->GetAngle()+1);
+			side->SetAngle(side->GetAngle()+1);
 		}
 		if(xbox->getX()){
-			up->SetAngle(side->GetAngle()-1);
+			side->SetAngle(side->GetAngle()-1);
 		}
 		std::cout<<"Current Y Angle: "<<up->GetAngle()<<" Current X Angle: "<<side->GetAngle()<<std::endl;
 
