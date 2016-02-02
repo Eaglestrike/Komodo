7a39bd2b796744a54c8ca73b876c8e03d2474f27
Ankith Uppunda <auppunda@github.com>
2016-02-01 19:19:56 -0800

    merge

---
+++ SampleDrive/src/modules/FlipperModule.cpp
@@ -0,0 +1,18 @@
+/*
+ * FlipperModule.cpp
+ *
+ *  Created on: Jan 21, 2016
+ *      Author: Samsung
+ */
+
+#include <modules/FlipperModule.h>
+
+FlipperModule::FlipperModule() {
+	// TODO Auto-generated constructor stub
+
+}
+
+FlipperModule::~FlipperModule() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ SampleDrive/src/modules/FlipperModule.h
@@ -0,0 +1,17 @@
+/*
+ * FlipperModule.h
+ *
+ *  Created on: Jan 21, 2016
+ *      Author: Samsung
+ */
+
+#ifndef SRC_MODULES_FLIPPERMODULE_H_
+#define SRC_MODULES_FLIPPERMODULE_H_
+
+class FlipperModule {
+public:
+	FlipperModule();
+	virtual ~FlipperModule();
+};
+
+#endif /* SRC_MODULES_FLIPPERMODULE_H_ */
