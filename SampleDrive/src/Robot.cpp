509eed5c1daa18d17de0dcb4535d59eba9b91b0c
Cole Brinsfield <gizmosrock@gmail.com>
2016-02-14 14:29:59 -0800

    really messy vision merge that should work

--- SampleDrive/src/Robot.cpp
+++ SampleDrive/src/Robot.cpp
@@ -16,7 +16,27 @@
 
 class Robot: public IterativeRobot
 {
+public:
+	std::shared_ptr<NetworkTable> visionTable;
 private:
+	double movementInX=0;
+	double xTime;
+	double yTime;
+	Xbox* xbox;
+	double xAngle;
+	Timer* xTimer;
+	Timer* yTimer;
+	double yAngle;
+	int ticks=0;
+	int yticks=0;
+	double movementInY=0;
+	double x;
+	double y;
+	LiveWindow *lw = LiveWindow::GetInstance();
+	double frameWidth,frameHeight,xMovePerTick,yMovePerTick,Xtolerance, Ytolerance;
+	int movementFactor=1;
+	Servo* up;
+	Servo* side;
 	LiveWindow *lw = LiveWindow::GetInstance();
 	SendableChooser *chooser;
 	const std::string autoNameDefault = "Default";
@@ -105,6 +125,18 @@ private:
 
 	void TeleopInit()
 	{
+		frameWidth = visionTable->GetNumber("width");
+		frameHeight = visionTable->GetNumber("height");
+		Xtolerance = visionTable->GetNumber("XTolerance");
+		Ytolerance = visionTable->GetNumber("YTolerance");
+		yMovePerTick = visionTable->GetNumber("yTicks");
+		xMovePerTick = visionTable->GetNumber("xTicks");
+		xTimer->Reset();
+		yTimer->Reset();
+		xTimer->Start();
+		yTimer->Start();
+		up->SetAngle(60);
+		side->SetAngle(0);
 		arcade = false;
 		intake->deployIntake();
 		//std::cout << "Autonomous starting" << std::endl;
@@ -179,7 +211,68 @@ private:
 
 		}
 		//intakeCounter++;
+		Xtolerance = visionTable->GetNumber("XTolerance");
+		Ytolerance = visionTable->GetNumber("YTolerance");
 
+		movementInX=0;
+		movementInY=0;
+		std::cout<<visionTable->GetNumber("detectedObject")<<std::endl;
+		if(visionTable->GetNumber("detectedObject")==1){
+			std::cout<<"ticks: "<<ticks<<std::endl;
+
+			x = visionTable->GetNumber("X");
+			y = visionTable->GetNumber("Y");
+
+			if(x>(frameWidth/2+Xtolerance)){
+				movementInX = -xMovePerTick;
+			}
+			if(x<(frameWidth/2-Xtolerance)){
+				movementInX = xMovePerTick;
+			}
+			if(y>(frameHeight/2+Ytolerance)){
+				movementInY=yMovePerTick;
+			}
+			else if(y<(frameHeight/2-Ytolerance)){
+				movementInY=-yMovePerTick;
+			}
+//			0.19sec/60degrees move speed on servos
+			if(ticks%15==0){
+				xAngle = visionTable->GetNumber("xAngle");
+				double moveAngle = (xAngle-26.4)*.5;
+				side->SetAngle(side->GetAngle()+moveAngle);
+				xTime = fabs((50/ 60)*moveAngle);
+				xTimer->Reset();
+				xTimer->Start();
+				ticks=0;
+			}
+			if(yticks%25==0){
+				yAngle = visionTable->GetNumber("yAngle");
+				double moveAngle = (19.8-yAngle);
+				up->SetAngle(up->GetAngle()+moveAngle);
+				yTime = fabs((50/60)*moveAngle);
+				yTimer->Reset();
+				yTimer->Start();
+				yticks=0;
+			}
+			if(up->GetAngle()>90){
+				up->SetAngle(90);
+			}
+			ticks++;
+			yticks++;
+		}
+		else{
+
+			std::cout<<"Sweeping"<<std::endl;
+			if(side->GetAngle()==180){
+				movementFactor=-1;
+			}
+			if(side->GetAngle()==0){
+				movementFactor=1;
+			}
+			std::cout<<side->GetAngle()<<std::endl;
+
+			side->SetAngle(side->GetAngle()+xMovePerTick*movementFactor*2);
+		}
 
 	}
 
